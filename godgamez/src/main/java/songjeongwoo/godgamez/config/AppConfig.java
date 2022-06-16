c4565dc3dcc0b79c19d5fc0339c53c78f654738f
송정우 <@DESKTOP-K73S629>
2022-06-16 12:34:24 +0900

    초기 프로젝트 생성

---
+++ godgamez/src/main/java/songjeongwoo/godgamez/config/AppConfig.java
@@ -0,0 +1,31 @@
+package songjeongwoo.godgamez.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("songjeongwoo.godgamez")
+@ImportResource("classpath:songjeongwoo/godgamez/config/app.xml")
+public class AppConfig implements WebMvcConfigurer {
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("main");
+	}
+}
