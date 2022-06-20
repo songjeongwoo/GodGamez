394b8f125cf7397fa2440e0fd90600ebef0102ab
jeongwoo <nice920525@naver.com>
2022-06-21 00:30:16 +0900

    로그아웃

--- godgamez/src/main/java/songjeongwoo/godgamez/web/UserController.java
+++ godgamez/src/main/java/songjeongwoo/godgamez/web/UserController.java
@@ -6,10 +6,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import songjeongwoo.godgamez.domain.User;
 import songjeongwoo.godgamez.service.UserService;
@@ -42,5 +44,13 @@ public class UserController {
 			return 0;
 		}
 		return result;
+	}
+	
+	/* 로그아웃 */
+	@GetMapping("/user/logout")
+	public ModelAndView logout(ModelAndView mv, HttpSession session) {
+		session.invalidate();
+		mv.setViewName("main");
+		return mv;
 	}
 }
